7b3c36dde9e52d069db965ab30488fad61a152c7
Hossen <mdmitho199911@gmail.com>
2022-05-26 01:47:44 +0600

    admin emila

--- index.js
+++ index.js
@@ -80,9 +80,19 @@ app.get("/parts", async (req, res) => {
     });
 
 
+    app.get("/admin/:email", async (req, res) => {
+      const email = req.params.email;
+      const user = await userCollection.findOne({ email: email });
+      const isAdmin = user.role === "admin";
+      res.send({ admin: isAdmin });
+    });
 
 
 
+
+
+
+  
 
   } 
   finally {
